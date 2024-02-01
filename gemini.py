13fe7c4e517ee7500e9a6e48cf9ba9c5e1f0da30
Khangphan <102360362+KhangPhan27@users.noreply.github.com>
2024-02-01 10:40:02 +0700

    Gemini using requests

--- gemini.py
+++ gemini.py
@@ -1,16 +1,30 @@
-import google.generativeai as genai
+import requests, json
 from os import environ
 from dotenv import load_dotenv
 from os.path import join, dirname
 # config Gemini model
 load_dotenv(join(dirname(__file__), ".env"))
-genai.configure(api_key=environ.get("GEMINI_API"))
-model = genai.GenerativeModel('gemini-pro')
-
-def gemini(content:str):
-    response = model.generate_content(contents=content)
-    return response.text
+api_key=environ.get("GEMINI_API")
 
 
+def gemini(message):
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={api_key}"
+    headers = {"Content-Type": "application/json"}
 
-    
+    data = {
+        "contents": [
+            {
+                "parts": [
+                    {
+                        "text": message
+                    }
+                ]
+            }
+        ]
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    try:
+        return response.json()["candidates"][0]["content"]["parts"][0]["text"]
+    except:
+        return "Đã có lỗi xảy ra!"
